9845398f2b23307f57fa9738c173f5d9df859f48
Tanay Raj <tanay.raj76@gmail.com>
2022-05-04 18:11:24 +0530

    latestTime store redis

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ const { client, redisConnection } = require("./connections/redis");
 
 redisConnection();
 
+// app.get("/api/getData", async (req, res) => {
+//   const c = await Content.find().sort({ contentTime: 1 });
+//   console.log(c);
+// });
+
 app.post("/api/addSubs", async (req, res) => {
   const { email, topic } = req.body;
   try {
@@ -36,15 +41,19 @@ app.post("/api/addContent", async (req, res) => {
   try {
     const contentDoc = await Content.create({
       contentText,
-      contentTime,
+      contentTime: new Date(contentTime),
       topic,
     });
-
+    console.log("Content Saved");
     //if contentTime is less than the value stored in redis then update the redis value
     const latestTime = await client.get("latestTime");
-
-    if (latestTime === null || contentTime < latestTime) {
-      await client.set("latestTime", contentTime);
+    const contentDateTime = new Date(contentTime);
+    console.log(contentDateTime);
+    const latestDateTime = new Date(latestTime);
+    console.log(latestDateTime);
+    if (latestTime === null || contentDateTime < latestDateTime) {
+      console.log("hey");
+      await client.set("latestTime", contentDateTime.toISOString());
       console.log("Saved");
     }
   } catch (e) {
